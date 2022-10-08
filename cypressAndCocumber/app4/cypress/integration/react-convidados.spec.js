5a1fb2d2c54ace3260586f220db9bed3084ad951
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-10-07 22:39:51 -0300

    app @v1.0.7

--- cypressAndCocumber/app4/cypress/integration/react-convidados.spec.js
+++ cypressAndCocumber/app4/cypress/integration/react-convidados.spec.js
@@ -7,10 +7,8 @@ describe('should be test correctly',()=>{
     it('should be insert data', ()=>{
         cy.visit('http://localhost:3000/')
 
-        cy.get("#inputNome")
-          .type('fulano')
-        cy.get("#inputIdade")
-          .type(15)
+        cy.get("#inputNome").type('fulano')
+        cy.get("#inputIdade").type(15)
 
         cy.get('.MuiButton-label').click()
     })
@@ -24,10 +22,8 @@ describe('should be test correctly',()=>{
     it('should be denie insert', ()=>{
         cy.visit('http://localhost:3000/')
 
-        cy.get("#inputNome")
-          .type('ful')
-        cy.get("#inputIdade")
-          .type(5)
+        cy.get("#inputNome").type('ful')
+        cy.get("#inputIdade").type(5)
 
         cy.get('.MuiButton-label').click()  
     })
