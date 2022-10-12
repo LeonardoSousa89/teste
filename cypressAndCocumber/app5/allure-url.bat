d46d7c4fb5f068fb425295ab7118003e8118cf9d
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-10-12 03:57:21 -0300

    app @v1.0.9

--- cypressAndCocumber/app5/allure-url.bat
+++ cypressAndCocumber/app5/allure-url.bat
@@ -1,3 +1,4 @@
+start https://www.youtube.com/watch?v=y3QZQF2wBW4&list=PLnUo-Rbc3jjyx5BVnG8MB7vNd5ecu2yP1&index=5
 start https://github.com/samlucax/youtube-cypress
 start https://www.npmjs.com/package/allure-commandline
 start https://www.npmjs.com/package/@shelex/cypress-allure-plugin
