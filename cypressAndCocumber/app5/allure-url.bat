c607929a61645409fc21f82a574614d32b8327a3
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-10-12 03:51:17 -0300

    app @v1.0.8

---
+++ cypressAndCocumber/app5/allure-url.bat
@@ -0,0 +1,4 @@
+start https://github.com/samlucax/youtube-cypress
+start https://www.npmjs.com/package/allure-commandline
+start https://www.npmjs.com/package/@shelex/cypress-allure-plugin
+start https://docs.qameta.io/allure-report/#_mocha
